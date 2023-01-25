3d88d4d08fc71203b67e1940373d7903fd527e08
Tu-LucD <tlduong123@gmail.com>
2023-01-25 00:39:31 -0500

    Did most of basic recycler view functionality

--- app/src/main/java/com/example/prmanager/ExerciseRVAdapter.kt
+++ app/src/main/java/com/example/prmanager/ExerciseRVAdapter.kt
@@ -1,4 +1,45 @@
 package com.example.prmanager
 
-class ExerciseRVAdapter (var list: List<ExerciseItems>, val){
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.lifecycle.ViewModel
+import androidx.recyclerview.widget.RecyclerView
+
+class ExerciseRVAdapter (
+    var list: List<ExerciseItems>,
+    val exerciseItemClickInterface: ExerciseItemClickInterface
+    ): RecyclerView.Adapter<ExerciseRVAdapter.ExerciseViewHolder>(){
+
+    inner class ExerciseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val nameTV = itemView.findViewById<TextView>(R.id.idTVItemName)
+        val repsTV = itemView.findViewById<TextView>(R.id.idTVNbReps)
+        val poundsTV = itemView.findViewById<TextView>(R.id.idTVNbPounds)
+        val deleteIV = itemView.findViewById<ImageView>(R.id.idTVDelete)
+    }
+    interface ExerciseItemClickInterface{
+        fun onItemClick(exerciseItems: ExerciseItems) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExerciseViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.exercise_rv_item,parent,false)
+        return ExerciseViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ExerciseViewHolder, position: Int) {
+        holder.nameTV.text = list.get(position).itemName
+        holder.repsTV.text = list.get(position).numberReps.toString()
+        holder.poundsTV.text = list.get(position).nbPounds.toString()
+        holder.deleteIV.setOnClickListener{
+            exerciseItemClickInterface.onItemClick(list.get(position))
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
 }
--- app/src/main/java/com/example/prmanager/MainActivity.kt
+++ app/src/main/java/com/example/prmanager/MainActivity.kt
@@ -1,17 +1,80 @@
 package com.example.prmanager
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ExerciseRVAdapter.ExerciseItemClickInterface {
     lateinit var itemsRV: RecyclerView
     lateinit var addFAB: FloatingActionButton
+    lateinit var list:List<ExerciseItems>
+    lateinit var exerciseRVAdapter: ExerciseRVAdapter
+    lateinit var exerciseViewModel: ExerciseViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         itemsRV = findViewById(R.id.idRVItems)
         addFAB = findViewById(R.id.idFABAdd)
+        list = ArrayList<ExerciseItems>()
+        exerciseRVAdapter = ExerciseRVAdapter(list,this)
+        itemsRV.layoutManager = LinearLayoutManager(this)
+        itemsRV.adapter = exerciseRVAdapter
+        val exerciseRepository = ExerciseRepository(ExerciseDatabase(this))
+        val factory = ExerciseViewModelFactory(exerciseRepository)
+        exerciseViewModel = ViewModelProvider(this,factory).get(ExerciseViewModel::class.java)
+        exerciseViewModel.getAllExerciseItems().observe(this, Observer {
+            exerciseRVAdapter.list = it
+            exerciseRVAdapter.notifyDataSetChanged()
+        })
+
+        addFAB.setOnClickListener{
+            openDialog()
+        }
+    }
+
+    fun openDialog(){
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.exercise_add_dialog)
+        val cancelBtn = dialog.findViewById<Button>(R.id.idBtnCancel)
+        val addBtn = dialog.findViewById<Button>(R.id.idBtnAdd)
+        val itemNameEdit = dialog.findViewById<EditText>(R.id.idEditItemName)
+        val itemRepsEdit = dialog.findViewById<EditText>(R.id.idEditNumberReps)
+        val itemPoundsEdit = dialog.findViewById<EditText>(R.id.idEditNumberPounds)
+        cancelBtn.setOnClickListener{
+            dialog.dismiss()
+        }
+        addBtn.setOnClickListener{
+            val itemName : String = itemNameEdit.text.toString()
+            val itemReps : String = itemRepsEdit.text.toString()
+            val itemPounds : String = itemPoundsEdit.text.toString()
+            val nbReps : Int = itemReps.toInt()
+            val nbPounds : Float = itemPounds.toFloat()
+            if(itemName.isNotEmpty() && itemReps.isNotEmpty() && itemPounds.isNotEmpty()){
+                val items = ExerciseItems(itemName,nbReps,nbPounds)
+                exerciseViewModel.insert(items)
+                Toast.makeText(applicationContext, "Exercise Added", Toast.LENGTH_SHORT).show()
+                exerciseRVAdapter.notifyDataSetChanged()
+                dialog.dismiss()
+            }
+            else{
+                Toast.makeText(applicationContext,"Missing Information", Toast.LENGTH_SHORT).show()
+            }
+        }
+        dialog.show()
+    }
+
+    override fun onItemClick(exerciseItems: ExerciseItems) {
+        exerciseViewModel.delete(exerciseItems)
+        exerciseRVAdapter.notifyDataSetChanged()
+        Toast.makeText(applicationContext,"Item Deleted..", Toast.LENGTH_SHORT).show()
     }
 }
